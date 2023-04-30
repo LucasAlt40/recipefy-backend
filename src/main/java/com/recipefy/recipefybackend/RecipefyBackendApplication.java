734876573dec6b2fec29fbccf0a30f7e4b4b9b90
LucasALT40 <lucas.alt40@gmail.com>
2023-04-29 21:49:59 -0300

    first commit

---
+++ src/main/java/com/recipefy/recipefybackend/RecipefyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recipefy.recipefybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipefyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipefyBackendApplication.class, args);
+    }
+
+}
